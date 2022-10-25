b0a43250a4552c27ebc413d610d54b1cd5b8b06f
Biranchi Parida <biranchi104@hotmail.com>
2022-10-25 22:55:19 +0530

    Added corrected code

--- LINQJOIN/LINQJOIN/JOIN/InnerJoin.cs
+++ LINQJOIN/LINQJOIN/JOIN/InnerJoin.cs
@@ -1,49 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using LINQJOIN.Common;
-using LINQJOIN.Model;
-using System.Linq;
+﻿	using System;
+	using System.Collections.Generic;
+	using LINQJOIN.Common;
+	using LINQJOIN.Model;
+	using System.Linq;
 
-namespace LINQJOIN.JOIN
-{
-	public class InnerJoin
+	namespace LINQJOIN.JOIN
 	{
-		List<Employee> em1 = FillListObject.FilEmployeeObject();
-		List<Employee2> em2 = FillListObject.FilEmployee2Object();
-
-		public void InnerjoininLambda()
+		public class InnerJoin
 		{
-			var result = em1.Join(em2,
-						   e1 => e1.EmpId,
-						   e2 => e2.EmpId,
-						   (e1, e2) => new
-						   {
-							   Employeename = e1.EmpName,
-							   EmployeeDept = e2.EmpDept
-						   });
-			foreach(var emp in result)
-            {
-				Console.WriteLine($"employee Name={emp.Employeename} & Employee Dept= {emp.EmployeeDept}");
-            }
+			List<Employee> em1 = FillListObject.FilEmployeeObject();
+			List<Employee2> em2 = FillListObject.FilEmployee2Object();
 
-		}
-		 public void NormalInnerJoin()
-        {
-			var result = from e1 in em1
-						 join e2 in em2
-						 on e1.EmpId equals e2.EmpId
-						 select new
-						 {
-							 EmployeeName = e1.EmpName,
-							 EmployeeDept = e2.EmpDept
-						 };
-			foreach (var emp in result)
+			public void InnerjoininLambda()
 			{
-				Console.WriteLine($"employee Name = {emp.EmployeeName} & Employee Dept = {emp.EmployeeDept}");
+				var result = em1.Join(em2,
+							   e1 => e1.EmpId,
+							   e2 => e2.EmpId,
+							   (e1, e2) => new
+							   {
+								   Employeename = e1.EmpName,
+								   EmployeeDept = e2.EmpDept
+							   });
+
+				foreach(var emp in result)
+				{
+					Console.WriteLine($"employee Name={emp.Employeename} & Employee Dept= {emp.EmployeeDept}");
+				}
+
+			}
+			public void NormalInnerJoin()
+			{
+				var result = from e1 in em1
+							 join e2 in em2
+							 on e1.EmpId equals e2.EmpId
+							 select new
+							 {
+								 EmployeeName = e1.EmpName,
+								 EmployeeDept = e2.EmpDept
+							 };
+
+				foreach (var emp in result)
+				{
+					Console.WriteLine($"employee Name = {emp.EmployeeName} & Employee Dept = {emp.EmployeeDept}");
+				}
 			}
 		}
+
+
 	}
-
-
-}
 
--- LINQJOIN/LINQJOIN/JOIN/LeftJoin.cs
+++ LINQJOIN/LINQJOIN/JOIN/LeftJoin.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using LINQJOIN.Common;
+using LINQJOIN.Model;
+using System.Linq;
+
 namespace LINQJOIN.JOIN
 {
 	public class LeftJoin
 	{
-		public LeftJoin()
-		{
+		List<Employee> em1 = FillListObject.FilEmployeeObject();
+		List<Employee2> em2 = FillListObject.FilEmployee2Object();
+
+		public void LeftJoinInNormal()
+        {
+			var result = from e1 in em1
+						 join e2 in em2
+						 on e1.EmpId equals e2.EmpId
+						 into EmployeeGroup
+						 from add in EmployeeGroup.DefaultIfEmpty()
+						 select new { add.EmpDept, add.EmpSalary };
+
+		    foreach (var emp in result)
+			{
+				Console.WriteLine($"employee Salary= {emp.EmpSalary} & Employee Dept= {emp.EmpDept}");
+			}
 		}
 	}
 }
